6260411bbcc267af6a3dfdd03c55122732ad453b
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-04-08 15:01:01 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import './css/style.css';
+import MainWrapper from "./components/MainWrapper";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/*<header className="App-header">*/}
+      <header>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+          Learn React with me
+        {/*</a>*/}
       </header>
+        <body>
+        <MainWrapper/>
+        </body>
+
     </div>
   );
 }
---
+++ src/components/MainWrapper.js
@@ -0,0 +1,10 @@
+function MainWrapper() {
+    return (
+        <div>
+            <p>ahoj</p>
+            <p>ahoj</p>
+        </div>
+    );
+}
+
+export default MainWrapper;
