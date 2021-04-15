0640d6ca694d86a75526e6ff1cb27efde03a31cb
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-04-15 18:24:33 +0100

    Updates

--- src/components/BlogsWrapper.js
+++
@@ -1,9 +0,0 @@
-export const BlogsWrapper = (props) => {
-    return (
-        <div className="blogsWrapper">
-            <h1>Articles</h1>
-            <p>Blog tips: title: h2; body: article</p>
-            {props.children}
-        </div>
-    );
-}
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-export const Header = () => {
-    return (
-        <div className="header">
-            <div><a href="">Home</a></div>
-            <div><a href="">About</a></div>
-            <div><a href="">Login</a></div>
-        </div>
-    );
-}
