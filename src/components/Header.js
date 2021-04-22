2c6d13fa47c463ffbc760ee1356eec0140453e65
Arthur91S <arturs.stulpe@gmail.com>
2021-04-22 12:19:03 +0100

    Header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 export const Header = () => {
     return (
         <div className="header">
+            <div className="logo"><a href="">SCSS</a></div>
             <div><a href="">Home</a></div>
             <div><a href="">About</a></div>
-            <div><a href="">Login</a></div>
+            <div><a href="" className="btn">Login</a></div>
         </div>
     );
 }
