8285833570de7f8964637a79c4b6ac875ae75fdb
Arthur91S <arturs.stulpe@gmail.com>
2021-04-22 15:08:34 +0100

    Added nav tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 export const Header = () => {
     return (
-        <div className="header">
+        <nav className="header">
             <div className="logo"><a href="">SCSS</a></div>
             <div><a href="">Home</a></div>
             <div><a href="">About</a></div>
             <div><button className="btn-primary">Login</button></div>
-        </div>
+        </nav>
     );
 }
