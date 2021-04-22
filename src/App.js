e72ae9cafb280ea2ef46df086310d7ec9089c4f9
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-04-22 15:13:09 +0100

    our implementation of ux/accessibility

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import {Blog} from "./components/Blog";
 import {Blog_SassIntro} from "./components/blogs/Blog_SassIntro";
 import {Blog_GitCoop} from "./components/blogs/Blog_GitCoop";
 import {Blog_SassMaps} from "./components/blogs/Blog_SassMaps";
-import {Blog_Accessibility} from "./components/blogs/Blog_Accessibility";
+import {Blog_AccessibilityGuidelines} from "./components/blogs/Blog_AccessibilityGuidelines";
+import {Blog_AccessibilityOurImplementation} from "./components/blogs/Blog_AccessibilityOurImplementation";
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
         <body>
         <Header/>
         <BlogsWrapper>
-            <Blog_Accessibility/>
+            <Blog_AccessibilityOurImplementation/>
+            <Blog_AccessibilityGuidelines/>
+
             <Blog author="Miro">
                 <h2>Does React and Sass have different purpose?</h2>
                 <article>I am not sure in React ans Sass are good friends as Sass is ment to style the whole scope, the whole project, but the idea behind react is to have kind
---
+++ src/components/blogs/Blog_AccessibilityOurImplementation.js
@@ -0,0 +1,11 @@
+import {Blog} from "../Blog";
+export const Blog_AccessibilityOurImplementation = () => {
+    return (
+        <Blog author="Miroslav" date="06/04/2021">
+            <h2>Accessibility - our implementation</h2>
+            <article className="center">
+                Checkout how our webpage looked like before making any ux/accessibility changes <a target="_blank" href="https://mmacode.github.io/sass_miro_and_arthur_before_ux_changes/" ><h2 style={{display:'inline'}}>here</h2></a>.
+            </article>
+        </Blog>
+    )
+}
