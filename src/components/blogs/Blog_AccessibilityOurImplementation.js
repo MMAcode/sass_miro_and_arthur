084f5a8d7572deee80606c1ed84c3e083d623319
Miroslav Makarov <miroslav.makarov@aexp.com>
2021-06-03 13:44:59 +0100

    responsive article updated

--- src/components/blogs/Blog_AccessibilityOurImplementation.js
+++ src/components/blogs/Blog_AccessibilityOurImplementation.js
@@ -3,8 +3,14 @@ export const Blog_AccessibilityOurImplementation = ({tabIndexForBlog}) => {
     return (
         <Blog author="Miroslav" date="06/04/2021" tabIndexForBlog={tabIndexForBlog}
               title="Accessibility - our implementation">
-            <article className="center">
-                Checkout how our webpage looked like before making any ux/accessibility changes <a target="_blank" href="https://mmacode.github.io/sass_miro_and_arthur_before_ux_changes/" ><h2 style={{display:'inline'}}>here</h2></a>.
+            <article>
+                <h5>Using TAB keyword</h5>
+                <p>To support visually inpaired people and make navigation on this page easier using TAB key, we changed 'tabIndex' of each blog's title into 0. By default, tab key would move user on blog's title, but with this implementation, it picks our titles.</p>
+                <p>Press TAB several times to experience this behaviour.</p>
+                <br/>
+                <p className="center">
+                    Checkout how our webpage looked like before making any ux/accessibility changes <a target="_blank" href="https://mmacode.github.io/sass_miro_and_arthur_before_ux_changes/" ><h2 style={{display:'inline'}}>here</h2></a>.
+                </p>
             </article>
         </Blog>
     )
--- src/components/blogs/Blog_Responsive.js
+++ src/components/blogs/Blog_Responsive.js
@@ -6,14 +6,14 @@ export const Blog_Responsive = ({tabIndexForBlog}) => {
               id="responsive"
               tabIndexForBlog={tabIndexForBlog}
               date="03/06/2021"
-              title="Responsive Design - using REM units (and @media)">
+              title="Responsive Design - using REM units and @media queries">
             <article>
-                <p>Default font size of everything os set to 20px</p>
-                <p>Title here has font size of 2rem; titles in other articles have fixed size of 20px.</p>
-                Title here has font size of 2rem; titles in other articles have fixed size of 20px.
-                <p>We are using media query with breaking point of width of 700px. To visualise this change, we change also the background of this web-page changes to light-yellow. </p>
+                <p>Default <strong>font size</strong> of everything ( html &#123; font-size:} ) is set to 20px, only blog title (h2) of all normal articles is set to 40px.</p>
+                <p>Using a <strong>@media query</strong>, when 'screen-width' is &lt;= 700px, default font size is changed to 10px, title/h2 size stays the same - 40px - and the background of this web-page changes to light-yellow).</p>
 
-                <br/>
+                <p><strong>Title/h2 of this article</strong> is using responsive "rem" units (h2=2rem). Notice that when the screen width changes across the 'breaking point' of 700px width, sizes of titles of other articles stay the same, but size of title of this article changes. This is due to the implemented 'rem' units. The actual size of this title/h2 is determined by current default size. Since we changed that size from orignal 20px to 10px, also all elements using rem units will change - will be halved. </p>
+                <p>(You may also notice that <strong>normal text</strong> size changes. Since we didn't define any specific size for 'article' or 'p' tags, it is using default 20px for big screen and 10px for small.)</p>
+                <p>This behaviour is implemented for demonstration purposes. Alternative/possibly better solution would be to make all/most of the elements use rem and other flexible units from the beginning.</p>
             </article>
         </Blog>
     )
